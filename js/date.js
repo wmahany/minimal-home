3f2457efd48eb67536f8ec3060769e6ca00ab429
Isaac Serrano <serranoie99@gmail.com>
2020-06-22 13:26:29 -0600

    Init commit with all project

---
+++ js/date.js
@@ -0,0 +1,9 @@
+function currentDate(){
+	n =  new Date();
+    y = n.getFullYear();
+    m = n.getMonth() + 1;
+    d = n.getDate();
+    document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
+}
+
+currentDate();
